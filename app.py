a051db5a1f9d479cbe5ca0416736761aa080b992
Emurotu Paul <paulesting@gmail.com>
2022-03-11 14:56:28 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, render_template, request
+import pandas as pd
+from fraud_detection.model.fraud_model import deploy_model
+app = Flask(__name__)
+model_path = os.path.join('fraud_detection', 'model', 'ml_model.dill.gz')
+MODEL = deploy_model(model_path)
+@app.route('/', methods=['GET'])
+def main():
+    return render_template('index.html')
+@app.route('/predict', methods=['POST'])
+def predict():
+    df = process_inputs()
+    y_proba = MODEL.predict_proba(df)
+
+    return f"Probability of fraud: {100 * y_proba[0, 1] :g} %"
+def process_inputs():
+    inputs = dict(request.form)
+    inputs.update({'user_id': 0, 'device_id': 0, 'ip_address': 0})
+
+    return pd.DataFrame(inputs,
+                        index=[0],
+                        columns=['user_id', 'signup_time', 'purchase_time',
+                                 'purchase_value', 'device_id', 'source',
+                                 'browser', 'sex', 'age', 'ip_address'])
+if __name__ == '__main__':
+    app.run()
